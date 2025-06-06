311a8bf9b2f4cc58023a4f6f89e25ba92011eaed
Vedant Kale <vedantcodes30@gmail.com>
2025-06-06 20:27:32 +0530

    'hidden scrollbar and fix header transition'

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,9 @@
 import { Link } from "next-view-transitions";
+import React from "react";
 
-export default function Header() {
+function Header() {
   return (
-    <header>
+    <header style={{ viewTransitionName: "header" }}>
       <div className="logo">
         <Link href="/">PersonaQuest</Link>
       </div>
@@ -32,67 +33,4 @@ export default function Header() {
     </header>
   );
 }
-
-// "use client";
-
-// import { useState } from "react";
-// import { Link } from "next-view-transitions";
-
-// export default function Header() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   return (
-//     <header className="p-4 pt-2 flex items-center justify-between md:px-8">
-//       <div className="logo text-xl font-medium">
-//         <Link href="/">PersonaQuest</Link>
-//       </div>
-
-//       {/* Hamburger Toggle (mobile only) */}
-//       <button
-//         onClick={() => setMenuOpen(!menuOpen)}
-//         className="text-xl md:hidden"
-//         aria-label="Toggle menu"
-//       >
-//         {menuOpen ? "✖" : "☰"}
-//       </button>
-
-//       {/* Desktop Nav */}
-//       <nav className="hidden md:block">
-//         <ul className="flex gap-6">
-//           <li>
-//             <Link href="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link href="/quiz">Quiz</Link>
-//           </li>
-//           <li>
-//             <Link href="/about">About</Link>
-//           </li>
-//         </ul>
-//       </nav>
-
-//       {/* Mobile Nav */}
-//       {menuOpen && (
-//         <nav className="absolute top-16 left-0 w-full  shadow-md md:hidden z-10">
-//           <ul className="flex flex-col items-center gap-4 py-4">
-//             <li>
-//               <Link href="/" onClick={() => setMenuOpen(false)}>
-//                 Home
-//               </Link>
-//             </li>
-//             <li>
-//               <Link href="/quiz" onClick={() => setMenuOpen(false)}>
-//                 Quiz
-//               </Link>
-//             </li>
-//             <li>
-//               <Link href="/about" onClick={() => setMenuOpen(false)}>
-//                 About
-//               </Link>
-//             </li>
-//           </ul>
-//         </nav>
-//       )}
-//     </header>
-//   );
-// }
+export default React.memo(Header);
